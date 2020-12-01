89334cdd106dd19418fea139ef107fee31a42907
Boris <borisperdija99@gmail.com>
2020-11-30 20:56:26 -0800

    Updating TB_URAM.vhd

--- tb_URAM.vhd
+++ tb_URAM.vhd
@@ -3,11 +3,10 @@ USE IEEE.STD_LOGIC_1164.all;
 USE ieee.numeric_std.ALL;
 USE ieee.std_logic_unsigned.ALL;
 
---testbench entities are always empty
-ENTITY tb_processing_element  IS
-END tb_processing_element;
+ENTITY tb_URAM  IS
+END tb_URAM;
 
-ARCHITECTURE behaviour of tb_processing_element IS
+ARCHITECTURE test of tb_URAM IS
 
 COMPONENT URAM is
   PORT(aclr		  : IN STD_LOGIC  := '0';
@@ -15,28 +14,98 @@ COMPONENT URAM is
 		  clock		  : IN STD_LOGIC  := '1';
 		  data		  : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
 		  rden		  : IN STD_LOGIC  := '1';
-		  wren		  : IN STD_LOGIC ;
-		  q	       	: OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
-	);
+		  wren		  : IN STD_LOGIC;
+		  q	       	: OUT STD_LOGIC_VECTOR (7 DOWNTO 0));
 END COMPONENT;
 
-  CONSTANT HALF_PERIOD : time := 10 ns;
-  SIGNAL tb_clock      : std_logic := '0';
-  SIGNAL tb_reset, tb_hard_reset, tb_ld, tb_ld_w  : std_logic;
-  SIGNAL tb_w_in, tb_a_in, tb_part_in, tb_partial_sum, tb_a_out : UNSIGNED(7 DOWNTO 0);
+  CONSTANT HALF_PERIOD              : time := 10 ns;
+  SIGNAL tb_clock                   : std_logic := '0';
+  SIGNAL tb_aclr, tb_rden, tb_wren  : std_logic;
+  SIGNAL tb_data, tb_q              :  STD_LOGIC_VECTOR(7 DOWNTO 0);
+  SIGNAL tb_address                 : STD_LOGIC_VECTOR(1 DOWNTO 0);
 
   BEGIN
-  DUT : processing_element
-  PORT MAP(clk => tb_clock, reset => tb_reset, hard_reset => tb_hard_reset, ld => tb_ld, ld_w => tb_ld_w, w_in => tb_w_in, a_in => tb_a_in, part_in => tb_part_in, partial_sum => tb_partial_sum, a_out => tb_a_out);
+  DUT : URAM
+  PORT MAP(aclr => tb_aclr, address => tb_address, clock => tb_clock, data => tb_data, rden => tb_rden, wren => tb_wren, q => tb_q);
 
   tb_clock <= NOT tb_clock AFTER HALF_PERIOD;
 
   PROCESS
   BEGIN
+
+  -- READ FROM SRAM AND WRITE VALUES TO SRAM
     wait for 5 ns;
 
+    tb_aclr <= '0';
+    tb_address <= "00";
+    tb_data <= "00000001";
+    tb_rden <= '0';
+    tb_wren <= '1';
+    wait for 100 ns;
+
+	 tb_address <= "00";
+    tb_data <= "00000001";
+	 tb_rden <= '1';
+    tb_wren <= '0';
+    wait for 40 ns;
+
+    tb_address <= "01";
+    tb_data <= "00000100";
+    tb_rden <= '0';
+    tb_wren <= '1';
+    wait for 20 ns;
+
+	 tb_address <= "01";
+    --tb_data <= "00000001";
+	 tb_rden <= '1';
+    tb_wren <= '0';
+    wait for 40 ns;
+
+	 tb_address <= "00";
+    --tb_data <= "00000001";
+	 tb_rden <= '1';
+    tb_wren <= '0';
+    wait for 40 ns;
 
 
+--    tb_address <= "11";
+--    tb_data <= "10000000";
+--    tb_rden <= '1';
+--    tb_wren <= '0';
+--    wait for 20 ns;
+--
+--    tb_address <= "11";
+--    tb_data <= "10000000";
+--    wait for 20 ns;
+--
+--    tb_address <= "11";
+--    tb_data <= "10000000";
+--    wait for 20 ns;
+--
+--    tb_address <= "11";
+--    tb_data <= "10000000";
+--    wait for 20 ns;
+--
+--    tb_address <= "00";
+--    tb_data <= "00000010";
+--    tb_rden <= '0';
+--    tb_wren <= '1';
+--    wait for 20 ns;
+--
+--    tb_address <= "00";
+--    tb_data <= "00000010";
+--    tb_rden <= '0';
+--    tb_wren <= '1';
+--    wait for 20 ns;
+
+    --tb_aclr <= '0';
+  --  tb_address <= "11";
+  --  tb_data <= "00001100";
+  --  wait for 20 ns;
+
+    --tb_address <= "00";
+    --tb_data <= "00000001";
+    --wait for 20 ns;
 
   END PROCESS;
-END behaviour;
+END test;
