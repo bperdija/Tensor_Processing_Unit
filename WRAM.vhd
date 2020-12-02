72c59126127b0434664149e5ed9799b881ba8e47
Laura <lcaflood@gmail.com>
2020-12-01 18:59:24 -0800

    updated tb_MMU and WRAM

--- WRAM.vhd
+++ WRAM.vhd
@@ -8,11 +8,11 @@ ENTITY WRAM IS
 	PORT
 	(
 		aclr		: IN STD_LOGIC  := '0';
-		address		: IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+		address	: IN STD_LOGIC_VECTOR (1 DOWNTO 0);
 		clock		: IN STD_LOGIC  := '1';
 		data		: IN STD_LOGIC_VECTOR (23 DOWNTO 0);
 		rden		: IN STD_LOGIC  := '1';
-		wren		: IN STD_LOGIC ;
-		q		: OUT STD_LOGIC_VECTOR (23 DOWNTO 0)
+		wren		: IN STD_LOGIC;
+		q		    : OUT STD_LOGIC_VECTOR (23 DOWNTO 0)
 	);
 END WRAM;
--- tb_MMU.vhd
+++ tb_MMU.vhd
@@ -1,40 +1,85 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.all;
 USE ieee.numeric_std.ALL;
-USE ieee.std_logic_unsigned.ALL;
+--USE ieee.std_logic_unsigned.ALL;
 
-ENTITY tb_URAM  IS
-END tb_URAM;
+ENTITY tb_MMU  IS
+END tb_MMU;
 
-ARCHITECTURE test of tb_URAM IS
+ARCHITECTURE test of tb_MMU IS
 
-COMPONENT URAM is
-  PORT(aclr		  : IN STD_LOGIC  := '0';
-		  address		: IN STD_LOGIC_VECTOR (1 DOWNTO 0);
-		  clock		  : IN STD_LOGIC  := '1';
-		  data		  : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-		  rden		  : IN STD_LOGIC  := '1';
-		  wren		  : IN STD_LOGIC;
-		  q	       	: OUT STD_LOGIC_VECTOR (7 DOWNTO 0));
+COMPONENT MMU is
+  PORT(clk, reset, hard_reset, ld, ld_w, stall  	  : IN STD_LOGIC;
+       a0, a1, a2                                   : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+       w0, w1, w2                                   : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+  	   y0, y1, y2 				                          : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
 END COMPONENT;
 
-  CONSTANT HALF_PERIOD              : time := 10 ns;
-  SIGNAL tb_clock                   : std_logic := '0';
-  SIGNAL tb_aclr, tb_rden, tb_wren  : std_logic;
-  SIGNAL tb_data, tb_q              :  STD_LOGIC_VECTOR(7 DOWNTO 0);
-  SIGNAL tb_address                 : STD_LOGIC_VECTOR(1 DOWNTO 0);
+  CONSTANT HALF_PERIOD                                         : time := 10 ns;
+  SIGNAL tb_clock                                              : std_logic := '0';
+  SIGNAL tb_hard_reset, tb_reset, tb_ld, tb_ld_w, tb_stall     : std_logic;
+  SIGNAL tb_a0, tb_a1, tb_a2, tb_y0, tb_y1, tb_y2              : STD_LOGIC_VECTOR(7 DOWNTO 0);
+  SIGNAL tb_w0, tb_w1, tb_w2                                   : STD_LOGIC_VECTOR(7 DOWNTO 0);
 
   BEGIN
-  DUT : URAM
-  PORT MAP(aclr => tb_aclr, address => tb_address, clock => tb_clock, data => tb_data, rden => tb_rden, wren => tb_wren, q => tb_q);
+
+  DUT : MMU
+  PORT MAP(clk => tb_clock, reset => tb_reset, hard_reset => tb_hard_reset, ld => tb_ld, ld_w => tb_ld_w, stall => tb_stall, a0 => tb_a0, a1 => tb_a1, a2 => tb_a2, w0 => tb_w0, w1 => tb_w1, w2 => tb_w2, y0 => tb_y0, y1 => tb_y1, y2 => tb_y2);
 
   tb_clock <= NOT tb_clock AFTER HALF_PERIOD;
 
   PROCESS
   BEGIN
-
     wait for 5 ns;
 
+    tb_hard_reset <= '1';
+    tb_reset <= '0';
+    tb_stall <= '0';
+    wait for 20 ns;
 
-  END PROCESS;
+    tb_ld_w <= '1';
+    tb_ld <= '0';
+    tb_w0 <= "00000001";
+    tb_w1 <= "00000011";
+    tb_w2 <= "00000101";
+    tb_a0 <= "00000000";
+    tb_a1 <= "00000000";
+    tb_a2 <= "00000000";
+    wait for 20 ns;
+
+    tb_ld_w <= '0';
+    tb_ld <= '1';
+    tb_w0 <= "00000000";
+    tb_w1 <= "00000000";
+    tb_w2 <= "00000000";
+    tb_a0 <= "00000010";
+    tb_a1 <= "00000000";
+    tb_a2 <= "00000000";
+    wait for 20 ns;
+
+    tb_a0 <= "00000100";
+    tb_a1 <= "00000110";
+    tb_a2 <= "00000000";
+    wait for 20 ns;
+
+    tb_a0 <= "00000110";
+    tb_a1 <= "00000010";
+    tb_a2 <= "00000100";
+    wait for 20 ns;
+
+    tb_a0 <= "00000000";
+    tb_a1 <= "00000100";
+    tb_a2 <= "00000110";
+    wait for 20 ns;
+
+    tb_a0 <= "00000000";
+    tb_a1 <= "00000000";
+    tb_a2 <= "00000010";
+    wait for 20 ns;
+
+
+    -- all output now
+
+
+    END PROCESS;
 END test;
