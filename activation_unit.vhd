d9fee21caa5980a1277b6f359efbb7bd4f36a65e
Laura <lcaflood@gmail.com>
2020-12-03 14:02:01 -0800

    updated au and added tb au files

--- activation_unit.vhd
+++ activation_unit.vhd
@@ -4,98 +4,111 @@ USE ieee.numeric_std.all;
 USE work.systolic_package.all;
 
 ENTITY Activation_Unit IS
-PORT(clk,reset, hard_reset, stall, done  	 : IN STD_LOGIC;
-     y_in0, y_in1, y_in2                   : IN UNSIGNED(7 DOWNTO 0);
-     row0, row1, row2                      : OUT UNSIGNED(7 DOWNTO 0)); -- is this others okay??
+PORT(clk,reset, hard_reset  : IN STD_LOGIC;
+     stall                  : IN STD_LOGIC := '0';
+     y_in0, y_in1, y_in2    : IN UNSIGNED(7 DOWNTO 0);
+	   done 						      : OUT STD_LOGIC;
+     row0, row1, row2       : OUT bus_width);
 END Activation_Unit;
 
-
+-- Will we always reset?
 ARCHITECTURE behaviour OF Activation_Unit IS
 
-TYPE state_type is (idle, state_0, state_1, state_2, state_3, state_4, state_5);
-SIGNAL next_state, current_state : state_type;
-SIGNAL s_counter : INTEGER := 0;  -- sets counter to zero
+TYPE state_type is (state0_0, state0_1, state0_2, state_1, state_2, state_3, state_4, state_5);
+SIGNAL next_state : state_type;
 SIGNAL y11, y12, y13, y21, y22, y23, y31, y32, y33 : UNSIGNED(7 DOWNTO 0) := (others => '0');
+SIGNAL sig_done : STD_LOGIC;
 
 BEGIN
-  PROCESS(stall, hard_reset, reset) -- possibly need s_counter in the process
+  PROCESS(reset, hard_reset, clk)
   BEGIN
-    IF (hard_reset = '1' OR reset = '1') THEN
-        s_counter <= (others => '0');
-        done <= '0';
-        y11 <= '0';
-        y12 <= '0';
-        y13 <= '0';
-        y21 <= '0';
-        y22 <= '0';
-        y23 <= '0';
-        y31 <= '0';
-        y32 <= '0';
-        y33 <= '0';
-        row0 <= (others => '0');
-        row1 <= (others => '0');
-        row2 <= (others => '0');
-        next_state <= idle;
+    IF (reset = '1' OR hard_reset = '1') then
+      next_state <= state0_0;
 
-    ELSIF (stall = '1') THEN
-        next_state <= current_state;
+    ELSIF (stall = '1') then
+        next_state <= next_state;
 
-    ELSE
-      CASE current_state IS
-  	      WHEN state_0 =>
-            done <= '0';
-  					s_counter <= s_counter + '1';
-  			 		IF (s_counter = 2) then
-  						next_state <= state_1;
-  					ELSE
-  						next_state <= state_0;
-  					END IF;
+    ELSIF (Rising_Edge(clk)) THEN
+      CASE next_state IS
+        WHEN state0_0 =>
+          next_state <= state0_1;
 
-  	      WHEN state_1 =>
-            y11 <= y_in0;
-  					next_state <= state_2;
+        WHEN state0_1 =>
+          next_state <= state0_2;
 
-  	      WHEN state_2 =>
-            y12 <= y_in0;
-            y21 <= y_in1;
-  					next_state <= state_3;
+        WHEN state0_2 =>
+          next_state <= state_1;
 
-  	      WHEN state_3 =>
-            y13 <= y_in0;
-            y22 <= y_in1;
-            y31 <= y_in2;
-  					next_state <= state_4;
+        WHEN state_1 =>
+          next_state <= state_2;
 
-  	     WHEN state_4 =>
-           y23 <= y_in1;
-           y32 <= y_in2;
-  				next_state <= state_5;
+        WHEN state_2 =>
+        next_state <= state_3;
 
-  	      WHEN state_5 =>
-            y33 <= y_in2;
-            done <= '1';
-  					next_state <= state_6;
-			  END CASE;
-      END IF;
+        WHEN state_3 =>
+            next_state <= state_4;
 
-		 IF (done = '1') THEN
-			row0(0) <= y11;
-			row0(1) <= y12;
-			row0(2) <= y13;
-			row1(0) <= y21;
-			row1(1) <= y22;
-			row1(2) <= y23;
-			row2(0) <= y31;
-			row2(1) <= y32;
-			row2(2) <= y33;
-		 END IF;
+        WHEN state_4 =>
+            next_state <= state_5;
 
-    END PROCESS;
+        WHEN state_5 =>
+          next_state <= state0_0;
+      END CASE;
+    END IF;
+  END PROCESS;
 
-    PROCESS(clk)
-    BEGIN
-      IF (Rising_Edge(clk)) THEN
-        current_state <= next_state;
-      END IF;
-    END PROCESS;
+  PROCESS(next_state) --inputs would also go here
+  BEGIN
+    CASE next_state IS
+    -- WHEN state0_0 OR state0_1
+      WHEN state0_0  =>
+        IF (sig_done = '1') THEN
+          row0(0) <= y11;
+          row0(1) <= y12;
+          row0(2) <= y13;
+          row1(0) <= y21;
+          row1(1) <= y22;
+          row1(2) <= y23;
+          row2(0) <= y31;
+          row2(1) <= y32;
+          row2(2) <= y33;
+        END IF;
+
+        sig_done <= '0';
+
+      WHEN state0_1 =>
+        sig_done <= '0';
+
+      WHEN state0_2 =>
+        sig_done <= '0';
+
+      WHEN state_1 =>
+        sig_done <= '0';
+        y11 <= y_in0;
+
+      WHEN state_2 =>
+        sig_done <= '0';
+        y12 <= y_in0;
+        y21 <= y_in1;
+
+      WHEN state_3 =>
+        sig_done <= '0';
+        y13 <= y_in0;
+        y22 <= y_in1;
+        y31 <= y_in2;
+
+      WHEN state_4 =>
+        sig_done <= '0';
+        y23 <= y_in1;
+        y32 <= y_in2;
+
+      WHEN state_5 =>
+        sig_done <= '1';
+        y33 <= y_in2;
+
+    END CASE;
+
+    done <= sig_done;
+
+  END PROCESS;
 END behaviour;
---
+++ tb_activation_unit.vhd
@@ -0,0 +1,81 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE ieee.numeric_std.ALL;
+USE work.systolic_package.all;
+
+ENTITY tb_Activation_Unit IS
+END tb_Activation_Unit;
+
+ARCHITECTURE test of tb_Activation_Unit IS
+
+COMPONENT Activation_Unit IS
+PORT(clk,reset, hard_reset  : IN STD_LOGIC;
+     stall                  : IN STD_LOGIC := '0';
+     y_in0, y_in1, y_in2    : IN UNSIGNED(7 DOWNTO 0);
+	  done 						 : OUT STD_LOGIC;
+    row0, row1, row2        : OUT bus_width);
+END COMPONENT;
+
+  CONSTANT HALF_PERIOD                                : time := 10 ns;
+  SIGNAL tb_clock                                     : std_logic := '0';
+  SIGNAL tb_reset, tb_hard_reset, tb_stall, tb_done   : std_logic;
+  SIGNAL tb_y_in0, tb_y_in1, tb_y_in2                 : UNSIGNED(7 DOWNTO 0);
+  SIGNAL tb_row0, tb_row1, tb_row2                    : bus_width;
+
+
+  BEGIN
+
+  DUT : Activation_Unit
+  PORT MAP(clk => tb_clock, reset => tb_reset, hard_reset => tb_hard_reset, stall => tb_stall, y_in0 => tb_y_in0, y_in1 => tb_y_in1, y_in2 => tb_y_in2, done => tb_done, row0 => tb_row0, row1 => tb_row1, row2 => tb_row2);
+
+  tb_clock <= NOT tb_clock AFTER HALF_PERIOD;
+
+  PROCESS
+  BEGIN
+
+    tb_reset <= '1';
+    tb_hard_reset <= '1';
+    tb_stall <= '0';
+    tb_y_in0 <= "00000000";
+    tb_y_in1 <= "00000000";
+    tb_y_in2 <= "00000000";
+    wait for 15 ns;
+
+	  tb_reset <= '0';
+    tb_hard_reset <= '0';
+	  wait for 50 ns;
+
+    tb_y_in0 <= "00000001";
+    tb_y_in1 <= "00000000";
+    tb_y_in2 <= "00000000";
+    wait for 20 ns;
+
+    tb_y_in0 <= "00000010";
+    tb_y_in1 <= "00000100";
+    tb_y_in2 <= "00000000";
+    wait for 20 ns;
+
+    tb_y_in0 <= "00000011";
+    tb_y_in1 <= "00000101";
+    tb_y_in2 <= "00000111";
+    wait for 20 ns;
+
+    tb_stall <= '1';
+    tb_y_in0 <= "00000011";
+    tb_y_in1 <= "00000101";
+    tb_y_in2 <= "00000111";
+    wait for 20 ns;
+
+    tb_stall <= '0';
+    tb_y_in0 <= "00000000";
+    tb_y_in1 <= "00000110";
+    tb_y_in2 <= "00001000";
+    wait for 20 ns;
+
+    tb_y_in0 <= "00000000";
+    tb_y_in1 <= "00000000";
+    tb_y_in2 <= "00001001";
+    wait for 2000 ns;
+
+  END Process;
+END test;
