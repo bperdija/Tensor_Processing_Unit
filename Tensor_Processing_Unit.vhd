d6af339e74cf6a28c6e41531f0941cd34290b1c2
Laura <lcaflood@gmail.com>
2020-12-04 01:26:49 -0800

    update TPU and tb

--- Tensor_Processing_Unit.vhd
+++ Tensor_Processing_Unit.vhd
@@ -1,13 +1,14 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.all;
 USE ieee.numeric_std.all;
---USE work.systolic_package.all;
+USE work.systolic_package.all;
 
 ENTITY Tensor_Processing_Unit IS
-PORT(clk,reset, hard_reset, setup, GO  : IN STD_LOGIC;
+PORT(clk, reset, hard_reset, setup     : IN STD_LOGIC;
+     GO                                : IN STD_LOGIC := '0';
      stall                             : IN STD_LOGIC := '0';
-     weights, a_in                     : IN UNSIGNED(23 DOWNTO 0);
-	   done 						                 : OUT STD_LOGIC;
+     weights, a_in                     : IN STD_LOGIC_VECTOR(23 DOWNTO 0);
+	   done 						                 : OUT STD_LOGIC := '0';
      y0, y1, y2                        : OUT bus_width);
 END Tensor_Processing_Unit;
 
@@ -43,64 +44,140 @@ PORT(clk, reset, hard_reset, ld, ld_w, stall  	  : IN STD_LOGIC;
 END COMPONENT;
 
 COMPONENT Activation_Unit IS
-PORT(clk,reset, hard_reset, GO_store_matrix  : IN STD_LOGIC;
+PORT(clk, reset, hard_reset, GO_store_matrix  : IN STD_LOGIC;
      stall                  : IN STD_LOGIC := '0';
      y_in0, y_in1, y_in2    : IN UNSIGNED(7 DOWNTO 0);
 	   done 						      : OUT STD_LOGIC;
      row0, row1, row2       : OUT bus_width);
 END COMPONENT;
 
-TYPE state_type is (idle, load_row0, load_row1, load_row2);
-SIGNAL next_state, current_state: state_type;
-SIGNAL ld_row0, ld_row1, ld_row2 : STD_LOGIC;
-SIGNAL W_out : STD_LOGIC_VECTOR (23 DOWNTO 0);
-SIGNAL element_address : UNSIGNED(1 DOWNTO 0);
---SIGNAL element_address_URAM : UNSIGNED(1 DOWNTO 0);
-SIGNAL a0, a1, a2 : UNSIGNED(7 DOWNTO 0);
-SIGNAL a_in0, a_in1, a_in2 : UNSIGNED(7 DOWNTO 0);
+TYPE state_type is (idle, load_row0, load_row1, load_row2, idle2, load_a1, load_a2, load_a3, load_a4, load_a5);
+SIGNAL next_state, current_state, next_state2, current_state2  : state_type; -- next_state and current_state are used for the setup and go, while next_state2 and current_state2 are used fortje computation + activation at the MMU
+SIGNAL URAM_reset, store_matrix, weight_ld                     : STD_LOGIC := '0';
+SIGNAL W_out                                                   : STD_LOGIC_VECTOR(23 DOWNTO 0);
+SIGNAL W_out0, W_out1, W_out2                                  : UNSIGNED(7 DOWNTO 0);
+SIGNAL element_address0, element_address1, element_address2    : STD_LOGIC_VECTOR(1 DOWNTO 0);
+SIGNAL a0, a1, a2, a_in0, a_in1, a_in2, MMU_y0, MMU_y1, MMU_y2 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (others => '0');
 
 BEGIN
 
   -- setup mode
-  PROCESS(current_state, hard_reset, reset, stall, setup, GO)
+  PROCESS(current_state, setup, GO)
   BEGIN
-    IF (hard_reset = '1' OR reset = '1') THEN
-        next_state <= idle;
-
-    ELSIF (GO = '1') THEN -- assuming setup is off / setup and go are never the same
-
-    ELSE -- setup = 1 (and GO "happens to be" 0, Laura Flood circa 2020)
+    IF (GO = '1' OR setup = '1') THEN -- fail safe
       CASE current_state IS
         WHEN idle =>
-          element_address <= "00"; -- used for WRAM row and URAM
+
+        IF (GO = '1') then
+          weight_ld <= '1';
+        ELSIF (setup = '1') then
+          a_ld <= '1';
+        END IF;
+
+          element_address0 <= "00"; -- used for WRAM row and URAM
+          element_address1 <= "00";
+          element_address2 <= "00";
           next_state <= load_row0;
 
         WHEN load_row0 =>
-          element_address <= "01";
+          element_address0 <= "01";
+          element_address1 <= "01";
+          element_address2 <= "01";
           next_state <= load_row1;
 
         WHEN load_row1 =>
-          element_address <= "10";
+          element_address0 <= "10";
+          element_address1 <= "10";
+          element_address2 <= "10";
           next_state <= load_row2;
 
         -- Set all control and weight buffers to 0 for the next cycle once the FSM returns to Idle.
         WHEN load_row2 =>
+          IF (weight_ld = '1') then
+            weight_ld <= '0';
+            a_ld <= '1';
+          ELSIF (a_ld = '1') THEN
+            a_ld <= '0';
+          END IF;
+
           element_address <= "11"; -- ?????????????????????????
           next_state <= idle;
         END CASE;
       END IF;
     END PROCESS;
 
-    URAM_reset <= hard_reset OR reset;
+    PROCESS(current_state2)
+    BEGIN
+      CASE current_state2 IS
+        WHEN idle2 =>
+          element_address0 <= "01" ;
+          element_address1 <= "00";
+          element_address2 <= "00";
+
+        WHEN load_a1 =>
+          element_address0 <= "10";
+          element_address1 <= "01";
+          element_address2 <= "00";
+
+        WHEN load_a2 =>
+          element_address0 <= "11";
+          element_address1 <= "10";
+          element_address2 <= "01";
+
+        WHEN load_a3 =>
+          element_address0 <= "00";
+          element_address1 <= "11";
+          element_address2 <= "10";
+
+        WHEN load_a4 =>
+          element_address0 <= "00";
+          element_address1 <= "00";
+          element_address2 <= "11";
+
+        WHEN load_a5 =>
+          element_address0 <= "00";
+          element_address1 <= "00";
+          element_address2 <= "00";
+          
+      END CASE;
+    END PROCESS;
+
+    PROCESS(clk, hard_reset, reset, stall)
+    BEGIN
+      IF (hard_reset = '1' OR reset = '1') THEN
+        IF (hard_reset = '1') THEN
+          current_state <= idle;
+        END IF;
+          current_state2 <= idle2;
+
+      ELSIF (GO = '1' AND stall = '1') THEN
+        current_state <= next_state;
+        current_state2 <= next_state2;
+
+      ELSIF (Rising_Edge(clk)) THEN
+        current_state <= next_state;
+        current_state2 <= next_state2;
+      END IF;
+    END PROCESS;
+
+    URAM_reset <= (hard_reset OR reset);
+
     a_in0 <= a_in(23 DOWNTO 16);
     a_in1 <= a_in(15 DOWNTO 8);
     a_in2 <= a_in(7 DOWNTO 0);
 
-    WRAM1 : PORT MAP WRAM(aclr => hard_reset, address => element_address, clock => clk, data => weights, rden => GO, wren => setup, q => W_out);
+    WRAM1 : WRAM PORT MAP(aclr => hard_reset, address => element_address0, clock => clk, data => weights, rden => GO, wren => setup, q => W_out);
 
-    URAM1 : PORT MAP URAM(aclr => URAM_reset, address => element_address, clock => clk, data => a_in0, rden => GO, wren => setup, q => a0);
-    URAM2 : PORT MAP URAM(aclr => URAM_reset, address => element_address, clock => clk, data => a_in1, rden => GO, wren => setup, q => a1);
-    URAM3 : PORT MAP URAM(aclr => URAM_reset, address => element_address, clock => clk, data => a_in2, rden => GO, wren => setup, q => a2);
+    URAM1 : URAM PORT MAP(aclr => URAM_reset, address => element_address0, clock => clk, data => a_in0, rden => GO, wren => setup, q => a0);
+    URAM2 : URAM PORT MAP(aclr => URAM_reset, address => element_address1, clock => clk, data => a_in1, rden => GO, wren => setup, q => a1);
+    URAM3 : URAM PORT MAP(aclr => URAM_reset, address => element_address2, clock => clk, data => a_in2, rden => GO, wren => setup, q => a2);
+
+    W_out0 <= UNSIGNED(W_out(23 DOWNTO 16));
+    W_out1 <= UNSIGNED(W_out(15 DOWNTO 8));
+    W_out2 <= UNSIGNED(W_out(7 DOWNTO 0));
+
+    MMU : MMU PORT MAP(clk => clk, reset => reset, hard_reset => hard_reset, ld => a_ld , ld_w => weight_ld, stall => stall, a0 => a0, a1 => a1, a2 => a2, w0 => W_out0, w1 =>  W_out1, w2 =>  W_out2, y0 => MMU_y0, y1 => MM_y1, y2 => MMU_y2, collect_matrix => store_matrix);
+
 
 
 END behaviour;
--- tb_Tensor_Processing_Unit.vhd
+++ tb_Tensor_Processing_Unit.vhd
@@ -1,12 +1,62 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.all;
 USE ieee.numeric_std.all;
---USE work.systolic_package.all;
+USE work.systolic_package.all;
+
+ENTITY tb_Tensor_Processing_Unit IS
+END tb_Tensor_Processing_Unit;
+
+ARCHITECTURE test of tb_Tensor_Processing_Unit IS
 
 COMPONENT Tensor_Processing_Unit IS
-PORT(clk,reset, hard_reset, setup, GO  : IN STD_LOGIC;
+PORT(clk, reset, hard_reset, setup, GO  : IN STD_LOGIC;
      stall                             : IN STD_LOGIC := '0';
-     weights, a_in                     : IN UNSIGNED(23 DOWNTO 0);
+     weights, a_in                     : IN STD_LOGIC_VECTOR(23 DOWNTO 0);
 	   done 						                 : OUT STD_LOGIC;
      y0, y1, y2                        : OUT bus_width);
 END COMPONENT;
+
+CONSTANT HALF_PERIOD                                                 : time := 10 ns;
+SIGNAL tb_clock                                                      : std_logic := '0';
+SIGNAL tb_reset, tb_hard_reset, tb_setup, tb_GO, tb_stall, tb_done   : std_logic;
+SIGNAL tb_weights, tb_a_in                                           : STD_LOGIC_VECTOR(23 DOWNTO 0);
+SIGNAL tb_y0, tb_y1, tb_y2                                           : bus_width;
+
+BEGIN
+
+DUT : Tensor_Processing_Unit
+PORT MAP(clk => tb_clock, reset => tb_reset, hard_reset => tb_hard_reset, setup => tb_setup, GO => tb_go, stall => tb_stall, weights => tb_weights, a_in => tb_a_in, y0 => tb_y0, y1 => tb_y1, y2 => tb_y2);
+
+tb_clock <= NOT tb_clock AFTER HALF_PERIOD;
+
+-- WRAM      URAM (write elements down)
+-- 1 2 3     1 2 3
+-- 4 5 6     4 5 6
+-- 7 8 9     7 8 9
+
+PROCESS
+BEGIN
+
+tb_hard_reset <= '1';
+tb_reset <= '1';
+tb_stall <= '0';
+wait for 5 ns;
+
+tb_setup <= '1';
+tb_hard_reset <= '0';
+tb_reset <= '0';
+tb_weights <= "000000010000001000000011"; -- WRAM element 0
+tb_a_in <= "000000010000010000000111"; -- URAM 0
+wait for 20 ns;
+
+tb_weights <= "000001000000010100000110"; -- WRAM element 1
+tb_a_in <= "000000100000010100001000"; -- URAM 1
+wait for 20 ns;
+
+tb_weights <= "000001110000100000001001"; -- WRAM element 2
+tb_a_in <= "000000110000011000001001"; -- URAM 2
+wait for 20 ns;
+
+
+END PROCESS;
+end test;
